48328d76708935413f215feae66facad8ce41853
Kim Jisena <kimjisena@gmail.com>
2024-01-16 14:02:33 +0300

    misc: initial project set up

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen () {
+  return (
+    <View className="flex-1 bg-neutral-800">
+      { /* search bar and logo goes here */ }
+    </View>
+  );
+}
+
